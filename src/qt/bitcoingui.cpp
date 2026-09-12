b1b7b84598da2e9cc69921c27e2208b980791c80
Mark Roark <mroarkcrypto@users.noreply.github.com>
2026-09-13 00:25:08 +0300

    qt: update visible payment URI wording to azure

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -263,7 +263,7 @@ void BitcoinGUI::createActions()
     sendCoinsMenuAction->setToolTip(sendCoinsMenuAction->statusTip());
 
     receiveCoinsAction = new QAction(platformStyle->SingleColorIcon(":/icons/receiving_addresses"), tr("&Receive"), this);
-    receiveCoinsAction->setStatusTip(tr("Request payments (generates QR codes and bloodstone: URIs)"));
+    receiveCoinsAction->setStatusTip(tr("Request payments (generates QR codes and azure: URIs)"));
     receiveCoinsAction->setToolTip(receiveCoinsAction->statusTip());
     receiveCoinsAction->setCheckable(true);
     receiveCoinsAction->setShortcut(QKeySequence(Qt::ALT + Qt::Key_3));
@@ -356,7 +356,7 @@ void BitcoinGUI::createActions()
     usedReceivingAddressesAction->setStatusTip(tr("Show the list of used receiving addresses and labels"));
 
     openAction = new QAction(tr("Open &URI…"), this);
-    openAction->setStatusTip(tr("Open a bloodstone: URI"));
+    openAction->setStatusTip(tr("Open an azure: URI"));
 
     m_open_wallet_action = new QAction(tr("Open Wallet"), this);
     m_open_wallet_action->setEnabled(false);
